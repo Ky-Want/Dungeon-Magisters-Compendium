4f2a681263b9c287ddfe57a160a40800fa7bbe85
Lumine3449 <>
2023-02-23 14:06:57 -0700

    extra client deleted

--- dungeon-magisters-compendium/client/app/Models/Account.js
+++
@@ -1,9 +0,0 @@
-export class Account {
-  constructor(data) {
-    this.id = data.id
-    this.email = data.email
-    this.name = data.name
-    this.picture = data.picture
-    //Add additional properties if needed
-  }
-}
